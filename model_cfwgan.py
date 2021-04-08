6f80bfb2d22a3177828c0f50bb1cf959f085cd76
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2021-04-08 16:05:40 +0200

    LinearTower implemented + tests

--- model_cfwgan.py
+++ model_cfwgan.py
@@ -1,8 +1,28 @@
 from torch import nn
 import torch
+import math
+import itertools
+
+class LinearTower(nn.Module):
+    def __init__(self, input_size, output_size, num_hidden_layers):
+        super().__init__()
+        c = 2**math.floor(math.log2(input_size))
+        if c == input_size:
+            c //= 2
+
+        l = [(nn.Linear(c//2**i, c//2**(i+1)), nn.ReLU(True)) for i in range(num_hidden_layers-1)]
+        l.insert(0, (nn.Linear(input_size, c), nn.ReLU(True)))
+        l = list(itertools.chain(*l))
+        l.append(nn.Linear(c//2**(num_hidden_layers-1), output_size))
+        self.sequential = nn.Sequential(*l)
+
+    def forward(self, x):
+        return self.sequential(x)
+
 
 class Generator(nn.Module):
-    def __init__(self, num_items):
+    def __init__(self, num_items, num_layers):
         super().__init__()
         self.num_items = num_items
-        self.sequential =
+        c = math.floor(math.log2(num_items))
+        l = [nn.Linear()]
---
+++ tests/test_model_cfwgan.py
@@ -0,0 +1,16 @@
+import unittest
+from model_cfwgan import LinearTower
+import torch
+import numpy as np
+
+class MyTestCase(unittest.TestCase):
+    def test_linearTower(self):
+        model = LinearTower(16, 2, 3)
+        out = model(torch.tensor(np.random.rand(5,16), dtype=torch.float))
+        self.assertEqual(out.shape, (5, 2))
+        l = [(x.in_features, x.out_features) for x in model.sequential if isinstance(x, torch.nn.Linear)]
+        self.assertEqual(l, [(16, 8), (8, 4), (4,2), (2, 2)])
+
+
+if __name__ == '__main__':
+    unittest.main()
