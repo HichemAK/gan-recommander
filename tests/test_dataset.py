bd2a3ff871b8fe92bd23bc291f413a592ad2498e
ouss1002 <oboudjeniba@gmail.com>
2021-04-08 20:13:30 +0100

    Add more tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,6 +2,7 @@ import unittest
 from dataset import MovieLensDataset
 import scipy.sparse as sparse
 import numpy as np
+import torch
 
 class MyTestCase(unittest.TestCase):
     def test_MovieLensDataset(self):
@@ -9,8 +10,18 @@ class MyTestCase(unittest.TestCase):
         row  = [0,0,1,2,3,3]
         col  = [4,3,4,2,1,0]
         data = [1.,1.,1.,1.,1.,1.]
+
+        # test for matrix equality
         expected_mat = sparse.csr_matrix((data, (row, col)), shape=(max(row) + 1, max(col) + 1))
         self.assertTrue(np.array_equal(dataset.matrix.todense(), expected_mat.todense()))
+
+        # test for first item retrieval
+        expected_retrieval = torch.tensor([0., 0., 0., 1., 1.,])
+        self.assertTrue(torch.equal(dataset[0], expected_retrieval))
+
+        #test for last item retrieval
+        expected_retrieval = torch.tensor([1., 1., 0., 0., 0.,])
+        self.assertTrue(torch.equal(dataset[-1], expected_retrieval))
 
 if __name__ == '__main__':
     unittest.main()
