8073a13b7bf4255a1d857221b0862b7f5862273d
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2021-04-08 20:03:53 +0200

    Add MovieLens info

--- train.py
+++ train.py
@@ -7,11 +7,11 @@ import pytorch_lightning as pl
 
 batch_size = 16
 
-dataset = MovieLensDataset()
+dataset = MovieLensDataset('ml-latest-small/ratings.csv')
 train_size = int(len(dataset)*0.8)
 train, val = random_split(dataset, [train_size, len(dataset)-train_size], generator=torch.Generator().manual_seed(1234))
 
-model = CFWGAN(dataset.num_items)
+model = CFWGAN(dataset.item_count)
 trainer = pl.Trainer(max_epochs=10)
 trainer.fit(model, DataLoader(train, batch_size, shuffle=True), DataLoader(val, batch_size*2, shuffle=True))
 
