26f0c91033ee26a2b0582e45f31b2eb597fd1ff9
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2021-04-09 22:36:50 +0200

    Add model checkpoint

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import random_split, DataLoader
 
 from model_cfwgan import CFWGAN
@@ -12,6 +13,8 @@ train_size = int(len(dataset)*0.8)
 train, val = random_split(dataset, [train_size, len(dataset)-train_size], generator=torch.Generator().manual_seed(1234))
 
 model = CFWGAN(dataset.item_count, alpha=0.1, s_zr=0.7, s_pm=0.7)
+
+model_checkpoint = ModelCheckpoint(monitor='precision_at_5', save_top_k=5, save_weights_only=True)
 trainer = pl.Trainer(max_epochs=10)
 trainer.fit(model, DataLoader(train, batch_size, shuffle=True), DataLoader(val, batch_size*2))
 
