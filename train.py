10d949f655379f94a9d1c96cd5679ee2fd002b34
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2021-04-12 15:42:24 +0200

    Implemented manual torch loop

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import random_split, DataLoader
 
-from model_cfwgan import CFWGAN
+from model_cfwgan2 import CFWGAN
 from dataset2 import MovieLensDataset
 import torch
 import pytorch_lightning as pl
@@ -18,13 +18,14 @@ test_size = int(len(test)*0.5)
 
 test, val = random_split(dataset, [test_size, len(test)-test_size], generator=torch.Generator().manual_seed(1234))
 
-model = CFWGAN(train, dataset.item_count, alpha=0.1, s_zr=0.5, s_pm=0, d_steps=3, g_steps=5)
+model = CFWGAN(train, dataset.item_count, alpha=0.1, s_zr=0.5, s_pm=0.5, d_steps=3, g_steps=5)
+model.train(DataLoader(train, batch_size, shuffle=True), DataLoader(val, batch_size*2))
 
-model_checkpoint = ModelCheckpoint(monitor='precision_at_5', save_top_k=5, save_weights_only=True, mode='max',
-                                   filename='model-{epoch}-{precision_at_5:.4f}')
-
-trainer = pl.Trainer(max_epochs=100, callbacks=[model_checkpoint])
-trainer.fit(model, DataLoader(train, batch_size, shuffle=True), DataLoader(val, batch_size*2))
-model = CFWGAN.load_from_checkpoint(model_checkpoint.best_model_path, trainset=train, num_items=dataset.item_count)
-trainer.test(model, DataLoader(test, batch_size*2))
+# model_checkpoint = ModelCheckpoint(monitor='precision_at_5', save_top_k=5, save_weights_only=True, mode='max',
+#                                    filename='model-{epoch}-{precision_at_5:.4f}')
+#
+# trainer = pl.Trainer(max_epochs=100, callbacks=[model_checkpoint])
+# trainer.fit(model, DataLoader(train, batch_size, shuffle=True), DataLoader(val, batch_size*2))
+# model = CFWGAN.load_from_checkpoint(model_checkpoint.best_model_path, trainset=train, num_items=dataset.item_count)
+# trainer.test(model, DataLoader(test, batch_size*2))
 
