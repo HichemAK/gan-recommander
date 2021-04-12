9f7002875b07bca072b182797f5f0f1d6b95f485
ouss1002 <oboudjeniba@gmail.com>
2021-04-12 11:54:52 +0100

    Implement test for flipping

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
     def test_MovieLensDataset(self):
         dataset = MovieLensDataset(ratings_file='test_ratings.csv', movies_file='test_movies.csv')
         row  = [0,0,1,2,3,3]
-        col  = [4,3,4,2,1,0]
+        col  = [0,1,0,2,3,4]
         data = [1.,1.,1.,1.,1.,1.]
 
         # test for matrix equality
@@ -16,21 +16,21 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(np.array_equal(dataset.matrix.todense(), expected_mat.todense()))
 
         # test for first item retrieval
-        expected_retrieval = torch.tensor([0., 0., 0., 1., 1.,])
+        expected_retrieval = torch.tensor([1., 1., 0., 0., 0.,])
         self.assertTrue(torch.equal(dataset[0][0], expected_retrieval))
 
         # test for last item retrieval
-        expected_retrieval = torch.tensor([1., 1., 0., 0., 0.,])
+        expected_retrieval = torch.tensor([0., 0., 0., 1., 1.,])
         self.assertTrue(torch.equal(dataset[-1][0], expected_retrieval))
 
         # test for movie retrieval
         expected_string = "#1: Toy Story (1995) ===> Adventure|Animation|Children|Comedy|Fantasy"
-        self.assertTrue(dataset.get_movie(4) == expected_string)
+        self.assertTrue(dataset.get_movie(0) == expected_string)
 
         # test for movie list retrieval
         expected_str_list = "#1: Toy Story (1995) ===> Adventure|Animation|Children|Comedy|Fantasy\n" + \
             "#2: Jumanji (1995) ===> Adventure|Children|Fantasy"
-        self.assertTrue(dataset.get_movie_list_str([4,3]) == expected_str_list)
+        self.assertTrue(dataset.get_movie_list_str([0,1]) == expected_str_list)
 
     def test_split_train_test(self):
         dataset = MovieLensDataset(ratings_file='test_ratings.csv', movies_file='test_movies.csv')
